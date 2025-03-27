e31e832bcd1e8b38fee4f53647cb38dd4710cb2c
Isaac Donkor <justinkwakye50@gmail.com>
2025-03-27 15:25:52 +0000

    Initial commit of my C++ file

---
+++ 10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"Enter the first number"<<endl;
+    cin>>x;
+
+    cout<<"Enter the second number"<<endl;
+    cin>>y;
+
+    int larger=(x>y)?x:y;
+
+        cout<<"The larger number is :"<<larger<<endl;
+
+return 0;
+}
